57f07f81513ab9b9bda61af4ea67473c69c9ea51
Remzi Kılınç <remzikilnc46@gmail.com>
2023-10-18 22:10:57 +0300

    Created completed and incomplete badge components. Added add-delete button components and performed some preliminary preparations.

---
+++ src/components/button/delete-button/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DeleteButton = () => {
+    return (
+        <button className="text-xs bg-red-600 rounded px-2 py-1">Delete</button>
+    );
+};
+
+export default DeleteButton;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,62 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+import ComplatedBadge from "@/components/badge/complated-badge";
+import DeleteButton from "@/components/button/delete-button";
 
+const todos = [
+  {
+    id: 1,
+    title: 'Back End Developer',
+    details: 'EngineeringEngineeringEngineeringEngineeringEngineeringEngineeringEngineeringEngineeringEngineeringEngineeringEngineeringEngineeringEngineering',
+  },
+  {
+    id: 2,
+    title: 'Test Test TestTest',
+    details: 'Test TestTestTestTestTestTest Test Test',
+  },
+  {
+    id: 3,
+    title: 'Test ',
+    details: 'Remote Test Test Test Test  Test',
+  },
+]
+
+
+
+export default function Home() {
+
+  const handleClick = () => {
+    console.log('w')
+  }
+
+  return (
+    <main className="flex h-screen flex-col items-center justify-center">
+      <div className="overflow-hidden bg-gray-900 shadow sm:rounded-md max-w-3xl w-full">
+        <ul className="divide-y divide-gray-700">
+          {todos.map((todo) => (
+              <li key={todo.id}>
+                <div className="flex flex-row w-full justify-between">
+                  <div className="px-4 py-4 sm:px-6">
+                    <div className="flex items-center">
+                      <p className="truncate text-sm font-medium text-white">{todo.title}</p>
+                      <div className="ml-2 flex flex-shrink-0">
+                       <ComplatedBadge/>
+                      </div>
+                    </div>
+                    <div className="mt-2 sm:flex sm:justify-between">
+                      <div className="sm:flex">
+                        <p className="text-sm text-gray-500 text-ellipsis overflow-hidden whitespace-nowrap max-w-xl">
+                          {todo.details}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col justify-center items-center px-4 py-4 sm:px-6">
+                    <DeleteButton/>
+                  </div>
+                </div>
+              </li>
+          ))}
+        </ul>
+      </div>
     </main>
   )
 }
