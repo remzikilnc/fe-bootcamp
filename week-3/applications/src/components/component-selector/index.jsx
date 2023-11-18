a31c4cf7ac5923a9721484c73aeda4f63257725c
Remzi Kılınç <remzikilnc46@gmail.com>
2023-11-18 07:36:09 +0300

    changed img importing method for vercel bug

--- week-3/applications/src/components/component-selector/index.jsx
+++ week-3/applications/src/components/component-selector/index.jsx
@@ -1,33 +1,37 @@
 import ComponentList from "../component-list/index.jsx";
 import {WeekContainer} from "../week-container/index.jsx";
+import counterImg from '../../../component-img/counter.png';
+import coinFlipImg from '../../../component-img/coin-flip.png';
+import burgerImg from '../../../component-img/burger.png';
+import useStateTextComponentImg from '../../../component-img/use-state-text-component.png';
 
 const weeks = [
     {
         weekNumber: 3,
         components: [
             {
-                img: '../../../component-img/counter.png',
+                img: counterImg,
                 number: "1",
                 title: 'Counter',
                 desc: 'Counter component',
                 href: '/counter',
             },
             {
-                img: '../../../component-img/use-state-text-component.png',
+                img: useStateTextComponentImg,
                 number: "2",
                 title: 'Text',
                 desc: 'Text component',
                 href: '/use-state-text',
             },
             {
-                img: '../../../component-img/coin-flip.png',
+                img: coinFlipImg,
                 number: "3",
                 title: 'Coin Flip',
                 desc: 'Coin Flip component',
                 href: '/coin-flip',
             },
             {
-                img: '../../../component-img/coin-flip.png',
+                img: burgerImg,
                 number: "4",
                 title: 'Burger Application',
                 desc: 'Burger Application',
